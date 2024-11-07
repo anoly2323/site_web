4e4f3bec47f54aa06e84da8c442d7f958c0c0061
Samuel Diffio <samueldiffio06@gmail.com>
2024-11-07 15:06:55 +0100

    v1.3.0

--- calculatrice/index.js
+++ calculatrice/index.js
@@ -1,16 +1,13 @@
 function calculate(operation) {
-    // Récupération des valeurs des inputs
     let num1 = parseFloat(document.getElementById('num1').value);
     let num2 = parseFloat(document.getElementById('num2').value);
     let result = 0;
     
-    // Vérification des nombres valides
     if (isNaN(num1) || isNaN(num2)) {
         alert("Veuillez entrer deux nombres valides.");
         return;
     }
     
-    // Exécution de l'opération
     if (operation === '+') {
         result = num1 + num2;
     } else if (operation === '*') {
@@ -23,10 +20,8 @@ function calculate(operation) {
         result = num1 / num2;
     }
 
-    // Affichage du résultat
     document.getElementById('resultValue').textContent = result;
 
-    // Vérification de la parité
     if (result % 2 === 0) {
         document.getElementById('parity').textContent = "Parité: Pair";
     } else {
@@ -38,7 +33,6 @@ function swap() {
     let num1 = document.getElementById('num1').value;
     let num2 = document.getElementById('num2').value;
 
-    // Permuter les valeurs des deux cases
     document.getElementById('num1').value = num2;
     document.getElementById('num2').value = num1;
 }
